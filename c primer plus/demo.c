a0a35c92916b128d7ff87c8829a030b70c569983
Rabbit937 <hackrabbit@qq.com>
2023-03-30 19:14:12 +0800

    learn c primer plus

---
+++ c primer plus/demo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int dogs;
+    printf("How many dogs fo you have?\n");
+    scanf("%d", &dogs);
+    printf("So you have %d dog(s)!\n", dogs);
+    return 0;
+}
