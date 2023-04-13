5cf5162dc4f5dfed7d24f7c2d1ab78f9db57a5be
Rabbit937 <hackrabbit@qq.com>
2023-04-13 18:25:28 +0800

    feat: 分支和跳转

---
+++ c primer plus/cypher1.c
@@ -0,0 +1,21 @@
+// cypher1.c -- 更改输入，空格不变
+#include <stdio.h>
+#define SPACE ' ' // SPACE表示单引号-空格-单引号
+int main(void)
+{
+    char ch;
+
+    ch = getchar(); // 读取一个字符
+    while (ch != '\n')
+    {
+        if (ch == SPACE)
+            putchar(ch);
+        else
+            putchar(ch + 1);
+        ch = getchar(); 
+    }
+
+    putchar(ch);
+
+    return 0;
+}
---
+++ c primer plus/getchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = getchar();
+    printf("%c", ch);
+
+    return 0;
+}
